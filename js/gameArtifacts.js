d3eed6a1c9c1e947dffb03746c4e013a2534617d
Matthew Austin <maustin803@gmail.com>
2022-12-10 20:16:45 -0800

    Refactor gameArtifacts.js

--- js/gameArtifacts.js
+++ js/gameArtifacts.js
@@ -2,12 +2,10 @@
 
 // File contains game artifacts
 
-// BACKLOG: Update to have object literals for fruit options and friend options
-//          Update TAG #729
-const imageAssets = [['./img/assets/dog-banana.jpg', './img/assets/dragon-banana.jpg', './img/assets/kangaroo-banana.jpg', './img/assets/turtle-orange.jpg'], 
-                    ['./img/assets/dog-orange.jpg', './img/assets/dragon-orange.jpg', './img/assets/kangaroo-orange.jpg', './img/assets/turtle-orange.jpg'],
-                    ['./img/assets/dog-strawberry.jpg', './img/assets/dragon-strawberry.jpg', './img/assets/kangaroo-strawberry.jpg', './img/assets/turtle-strawberry.jpg'],
-                    ['./img/assets/dog-watermelon.jpg', './img/assets/dragon-watermelon.jpg', './img/assets/kangaroo-watermelon.jpg', './img/assets/turtle-watermelon.jpg']];
+// Board dimensions
+const BOARD_WIDTH = 4;
+const BOARD_HEIGHT = 4;
+const BOARD_AREA = BOARD_WIDTH * BOARD_HEIGHT;
 
 // Generates and returns a new GameBoard object
 function newGameBoard(){
@@ -16,16 +14,16 @@ function newGameBoard(){
 
 // Restores, refactors, and returns a GameBoard object from local storage
 function restoreGameBoard(){
-  let storedGameBoard = JSON.parse(localStorage.getItem(`savedGameBoardState`));
+  let storedGameBoard = JSON.parse(localStorage.getItem('savedGameBoardState'));
   let instantiatedGameBoard = new GameBoard;
-  let boardVersion
+  let boardVersion = instantiatedGameBoard.board;
 
-  for (i = 0; i < storedGameBoard.length; i++){
-    for (j = 0; j < storedGameBoard.length; j++){
-      let fruit = storedGameBoard[i][j][0];
-      let friend = storedGameBoard[i][j][1];
-      let imageSrc = storedGameBoard[i][j][2];
-      let occupiedBy = storedGameBoard[i][j][3];
+  for (let i = 0; i < storedGameBoard.length; i++){
+    for (let j = 0; j < storedGameBoard.length; j++){
+      let fruit = storedGameBoard[i][j].fruit;
+      let friend = storedGameBoard[i][j].friend;
+      let imageSrc = storedGameBoard[i][j].imageSrc;
+      let occupiedBy = storedGameBoard[i][j].occupiedBy;
 
       boardVersion[i][j] = new GameboardTile(fruit, friend, imageSrc, occupiedBy);
     }
@@ -35,7 +33,7 @@ function restoreGameBoard(){
 
 // Takes in a game board (2D) and saves it in local storage
 function saveGameBoard(board){
-  localStorage.setItem(`savedGameBoardState`, JSON.stringify(board));
+  localStorage.setItem('savedGameBoardState', JSON.stringify(board));
 }
 
 // Creates and returns a Players object which contains PlayerData
@@ -45,34 +43,35 @@ function newPlayers(){
 
 // Restores, refactors, and returns a Players object from local storage
 function restorePlayers(){
-  let storedPlayers = JSON.parse(localStorage.getItem(`savedPlayersState`));
+  let storedPlayers = JSON.parse(localStorage.getItem('savedPlayersState'));
   let instantiatedPlayers = new Players();
 
   for (let i = 0; i < storedPlayers.length; i++){
     let playerData = storedPlayers[i];
-    let wins = playerData[0];
-    let losses = playerData[1];
-    let playerToken = playerData[2];
+    let wins = playerData.wins;
+    let losses = playerData.losses;
+    let playerToken = playerData.playerToken;
     let instancedPlayerData = new PlayerData(wins, losses, playerToken);
-    instantiatedPlayers.push(instancedPlayerData);
+    instantiatedPlayers.players[i] = instancedPlayerData;
   }
-  
-  return instantiatedPlayers;  
+
+  return instantiatedPlayers;
 }
 
-// Takes in a Players object and stores it in local storage
+// Takes in a players array and stores it in local storage
 function savePlayers(players){
-  localStorage.setItem(`savedPlayersState`, JSON.stringify(players));
+  localStorage.setItem('savedPlayersState', JSON.stringify(players));
 }
 
 // Clears local saved data for players (wins/losses/assigned token)
 function resetPlayers(){
-  localStorage.clear(`savedPlayersState`);
+  localStorage.clear('savedPlayersState');
 }
 
 class GameBoard{
   constructor(){
     this.board = [];
+
     this.linearBoard = [];
     this.generate();
   }
@@ -82,7 +81,7 @@ class GameBoard{
     let intList  =[];
     let position = [];
 
-    for (let i = 0; i < 16; i++){
+    for (let i = 0; i < BOARD_AREA; i++){
       intList.push(i);
     }
 
@@ -91,23 +90,18 @@ class GameBoard{
       position.push(intList.splice(positionGenerator,1));
     }
 
-    // Loops may be updated to 'for in' if fruits and friends are defined in object literals
-    // TAG #729
-    for (let i = 0; i < 4; i++){
-      for (let j = 0; j < 4; j++){
-        let newTile = new GameboardTile(i, j, imageAssets[i][j]);
-        tileDeck.push([position.pop(), newTile]);
-      }
-    }
+    gameBoardTiles.forEach((gameBoardTile) => {
+      tileDeck.push([position.pop(), gameBoardTile]);
+    });
 
-    for (let i = 0; i < 4; i++){
-      this.board.push(new Array(4));
+    for (let i = 0; i < BOARD_HEIGHT; i++){
+      this.board.push(new Array(4).fill(0));
     }
 
     while (tileDeck.length > 0){
       let tile = tileDeck.pop();
-      let row = Math.floor(tile[0] / 4);
-      let column = tile[0] % 4;
+      let row = Math.floor(tile[0] / BOARD_WIDTH);
+      let column = tile[0] % BOARD_HEIGHT;
       this.board[row][column] = tile[1];
     }
 
@@ -138,12 +132,12 @@ class Players{
     if (playerToken1) {
       this.players[0].playerToken = playerToken1;
     } else {
-      this.players[0].playerToken = 'img/assets/black-token.svg';
+      this.players[0].playerToken = './img/assets/player-tokens/black-token.svg';
     }
     if (playerToken2) {
       this.players[1].playerToken = playerToken2;
     } else {
-      this.players[1].playerToken = 'img/assets/red-token.svg';
+      this.players[1].playerToken = './img/assets/player-tokens/red-token.svg';
     }
   }
 }
@@ -156,9 +150,25 @@ class PlayerData{
   }
 }
 
+const gameBoardTiles = [
+  new GameboardTile('banana', 'dog', './img/assets/tiles/banana-dog.jpg'),
+  new GameboardTile('banana', 'dragon', './img/assets/tiles/banana-dragon.jpg'),
+  new GameboardTile('banana', 'kangaroo', './img/assets/tiles/banana-kangaroo.jpg'),
+  new GameboardTile('banana', 'turtle', './img/assets/tiles/banana-turtle.jpg'),
+  new GameboardTile('orange', 'dog', './img/assets/tiles/orange-dog.jpg'),
+  new GameboardTile('orange', 'dragon', './img/assets/tiles/orange-dragon.jpg'),
+  new GameboardTile('orange', 'kangaroo', './img/assets/tiles/orange-kangaroo.jpg'),
+  new GameboardTile('orange', 'turtle', './img/assets/tiles/orange-turtle.jpg'),
+  new GameboardTile('strawberry', 'dog', './img/assets/tiles/strawberry-dog.jpg'),
+  new GameboardTile('strawberry', 'dragon', './img/assets/tiles/strawberry-dragon.jpg'),
+  new GameboardTile('strawberry', 'kangaroo', './img/assets/tiles/strawberry-kangaroo.jpg'),
+  new GameboardTile('strawberry', 'turtle', './img/assets/tiles/strawberry-turtle.jpg'),
+  new GameboardTile('watermelon', 'dog', './img/assets/tiles/watermelon-dog.jpg'),
+  new GameboardTile('watermelon', 'dragon', './img/assets/tiles/watermelon-dragon.jpg'),
+  new GameboardTile('watermelon', 'kangaroo', './img/assets/tiles/watermelon-kangaroo.jpg'),
+  new GameboardTile('watermelon', 'turtle', './img/assets/tiles/watermelon-turtle.jpg'),
+];
+
 // **************************
 // **** For Testing Only*****
 // **************************
-
-let test = new GameBoard;
-console.log(test);
